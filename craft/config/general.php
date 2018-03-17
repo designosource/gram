d54445d88e15eb91c622dbb30534ca95c868fae0
Caroline <carolineboeykens@Caro-Mac.local>
2018-03-17 18:43:32 +0100

    startpagina images

--- craft/config/general.php
+++ craft/config/general.php
@@ -10,13 +10,13 @@
 return array(
 
 	// Base site URL
-	'siteUrl' => null,
+	'siteUrl' => 'http://localhost:8888/gram/public/',
 
 	// Environment-specific variables (see https://craftcms.com/docs/multi-environment-configs#environment-specific-variables)
 	'environmentVariables' => array(),
 
 	// Default Week Start Day (0 = Sunday, 1 = Monday...)
-	'defaultWeekStartDay' => 0,
+	'defaultWeekStartDay' => 1,
 
 	// Enable CSRF Protection (recommended, will be enabled by default in Craft 3)
 	'enableCsrfProtection' => true,
@@ -28,6 +28,6 @@ return array(
 	'cpTrigger' => 'admin',
 
 	// Dev Mode (see https://craftcms.com/support/dev-mode)
-	'devMode' => false,
+	'devMode' => true,
 
 );
