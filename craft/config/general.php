6cd0823781a9ab0bbfe5f98b85dc4667397b2b0f
Caroline Boeykens <carolineboeykens@hotmail.com>
2018-05-27 12:21:00 +0200

    dev mode false

--- craft/config/general.php
+++ craft/config/general.php
@@ -28,7 +28,7 @@ return array(
 	'cpTrigger' => 'admin',
 
 	// Dev Mode (see https://craftcms.com/support/dev-mode)
-	'devMode' => true,
+	'devMode' => false,
 
 	//generate transforms before page load => no cpresources
 	'generateTransformsBeforePageLoad' => true,
